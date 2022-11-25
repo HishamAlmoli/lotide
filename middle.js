366cf5c711707a827e3ff92cad178ef3e095309f
HishamAlmoli <halmoli@my.centennialcollege.ca>
2022-11-24 20:22:58 -0500

    Refactor middle

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-    let result = true;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]){
-            result = false;
-            console.log(`result --> ${result}`);
-            return false;
-        }
-    }
-    console.log(`result --> ${result}`);
-    return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`Assertion Passed: ${actual} === ${expected} ✅✅✅`);
-      } else {
-        console.assert(actual === expected, `${actual} !== ${expected} 🛑🛑🛑`);
-      }
-  }
-  
-  // ACTUAL FUNCTION
   const middle = function(array) {
     let middle = [];
     if (array.length > 2) {
@@ -34,18 +12,6 @@ const assertArraysEqual = function(actual, expected) {
     }
     console.log(middle);
     return middle;
-  }
-  
-// TEST CODE
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+  };
 
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+  module.exports = middle;
