fae09db0a4a3ee4e079a93d77a82f7ff8e67db67
HishamAlmoli <halmoli@my.centennialcollege.ca>
2022-11-24 20:11:11 -0500

    Refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,11 @@
-const eqArrays = function(arr1, arr2) {
-    let result = true;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]){
-            result = false;
-            console.log(`result --> ${result}`);
-            return false;
-        }
-    }
-    console.log(`result --> ${result}`);
-    return true;
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log("PASSED: " + actual + "===" + expected + "\n");
+  } else {
+    console.log("Failed: " + actual + "!==" + expected + "\n");
+  }
 };
 
-// TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEqual;
